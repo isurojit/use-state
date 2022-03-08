eb6e24694ef659eb12872ddf04b6208020d83793
isurojit <iamsurojit@hotmail.com>
2022-03-08 17:56:32 +0530

    final Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <React.Fragment></React.Fragment>;
+  const [count, setCount] = useState(4);
+  //const [theme, setTheme] = useState("blue");
+
+  function decrementCount() {
+    setCount((preState) => {
+      return preState - 1;
+    });
+
+    // setTheme((preState) => {
+    //   return "red";
+    // });
+  }
+
+  function incrementCount() {
+    setCount((preState) => {
+      return preState + 1;
+    });
+
+    // setTheme((preState) => {
+    //   return "yellow";
+    // });
+  }
+
+  return (
+    <React.Fragment>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      {/* <span>{theme}</span> */}
+      <button onClick={incrementCount}>+</button>
+    </React.Fragment>
+  );
 }
 
 export default App;
